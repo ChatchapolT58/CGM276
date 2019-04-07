1ac4ec8e4084261fad9c7a7a0a2521e9d290246f
ChatchapolT58 <Chatchapol.t58@rsu.ac.th>
2019-04-07 12:06:49 +0700

    Multiplayer for Unity

---
+++ 04-multiplayer_game_1/client/Game1/Assets/Scripts/Game1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Game1 : MonoBehaviour
+{
+
+    static SocketIOComponent socket;
+
+    public GameObject playerPreFab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+
+        socket.On("spawn", OnSpawned);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnConnected(SocketIOEvent e)
+    {
+        print("connected");
+
+        socket.Emit("move");
+    }
+
+    void OnSpawned(SocketIOEvent e)
+    {
+        print("spawned");
+
+        Instantiate(playerPreFab);
+
+        //socket.Emit("spawn");
+    }
+}
--- 04-multiplayer_game_1/server/server.js
+++ 04-multiplayer_game_1/server/server.js
@@ -2,4 +2,26 @@ var io = require('socket.io')(process.env.PORT || 3000);
 
 console.log("server started")
 
-io.on("connection", function (socket) { console.log("cilent connected") });
+var playerCount = 0;
+
+io.on("connection", function (socket) {
+
+    console.log("cilent connected");
+
+    socket.broadcast.emit('spawn');
+    playerCount++;
+
+    for (i = 0; i < playerCount; i++) {
+        socket.emit("spawn");
+        console.log("spawn existing player");
+    }
+
+    socket.on("move", function (data) {
+        console.log("move");
+    });
+
+    socket.on("disconnect", function () {
+        console.log("client disconnect");
+        playerCount--;
+    });
+});
