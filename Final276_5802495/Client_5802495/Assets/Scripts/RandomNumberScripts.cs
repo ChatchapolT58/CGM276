ecf3fccb73e142e4cb085e9e8951419679634da5
ChatchapolT58 <Chatchapol.t58@rsu.ac.th>
2019-05-07 17:26:18 +0700

    Test267

---
+++ Final276_5802495/Client_5802495/Assets/Scripts/RandomNumberScripts.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SocketIO;
+
+public class RandomNumberScripts : MonoBehaviour
+{
+    [SerializeField] Button btnsend = null;
+    [SerializeField] InputField numberInput = null;
+    [SerializeField] Text answerText = null;
+
+    SocketIOComponent socketIOComponent = null;
+
+
+    void Awake()
+    {
+        socketIOComponent = GetComponent<SocketIOComponent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        socketIOComponent.On("return result", OnReturnResult);
+        socketIOComponent.On("new lottery create", OnNewLotteryCreated);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SendSuggestion()
+    {
+            int suggestNumber = 0;
+            try
+            {
+                suggestNumber = int.Parse(numberInput.text);
+            }
+            catch (System.InvalidCastException)
+            {
+                suggestNumber = 0;
+            }
+            string data = JsonUtility.ToJson(new PlayerJSON(PlayerData.name, suggestNumber));
+            socketIOComponent.Emit("request suggest", new JSONObject(data));
+    }
+
+    void OnReturnResult(SocketIOEvent socketIOEvent)
+    {
+        var result = CheckJSON.CreateFromJSON(socketIOEvent.data.ToString());
+
+        switch (result.status)
+        {
+            case -1:
+                answerText.text = "Too Low";
+                answerText.color = Color.red;
+                break;
+            case 0:
+                if (result.name == PlayerData.name)
+                {
+                    answerText.text = "You win the lottery";
+                    answerText.color = Color.green;
+                }
+                else
+                {
+                    answerText.text = $"{result.name} win the lottery";
+                    answerText.color = Color.yellow;
+                }
+                break;
+            case 1:
+                answerText.text = "Too High";
+                answerText.color = Color.red;
+                break;
+        }
+    }
+    void OnNewLotteryCreated(SocketIOEvent socketIOEvent)
+    {
+        btnsend.GetComponentInChildren<Text>().text = "New Game";
+    }
+
+    public class PlayerJSON
+    {
+        public string name;
+        public int number;
+
+        public PlayerJSON(string name, int number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+        public static PlayerJSON CreateFromJSON(string data)
+        {
+            return JsonUtility.FromJson<PlayerJSON>(data);
+        }
+    }
+    public class CheckJSON
+    {
+        public string name;
+        public int status;
+
+        public static CheckJSON CreateFromJSON(string data)
+        {
+            return JsonUtility.FromJson<CheckJSON>(data);
+        }
+    }
+
+    public static class PlayerData
+    {
+        public static string name;
+    }
+
+}
---
+++ Final276_5802495/Server_5802495/server.js
@@ -0,0 +1,53 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var lottery = getRandomInt(100);
+
+server.listen(3000, function () {
+    console.log("current lottery is " + lottery);
+});
+
+io.on('connection', function (socket) {
+
+    socket.on('request suggest', function (data) {
+        console.log(data.name + " has request: " + data.number);
+        if (data.number == lottery) {
+            var checkResult = {
+                name: data.name,
+                status: 0
+            }
+            socket.broadcast.emit('return result',checkResult);
+            socket.emit('return result',checkResult);
+            console.log(data.name+" win the lottery");
+            lottery = getRandomInt(100);
+            console.log("the new lottery has been created");
+            console.log("current lottery is " + lottery);
+            socket.emit('new lottery created');
+            socket.broadcast.emit('new lottery created');
+        }
+        else {
+            var resResult = 0;
+            if(data.number < lottery)
+            {
+                resResult = -1;
+            }
+            else
+            {
+                resResult = 1;
+            }
+            var checkResult={
+                name:data.name,
+                status:resResult
+            }
+            socket.emit('return result',checkResult);
+            console.log(data.name+" is not correct");
+        }
+    })
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+console.log("server is running");
