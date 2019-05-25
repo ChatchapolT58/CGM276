695d287ea366ec9206283839c7877a7ceb46d516
ChatchapolT58 <46855839+ChatchapolT58@users.noreply.github.com>
2019-05-25 22:58:07 +0700

    Final

---
+++ Project276_5802495/client/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject playerForm;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if(health != null)
+        {
+            health.TakeDamage(playerForm, 10);
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project276_5802495/client/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public bool isLocalPlayer = false;
+
+    Vector3 oldPosition;
+    Vector3 currentPosition;
+    Quaternion oldRotation;
+    Quaternion currentRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        oldPosition = transform.position;
+        currentPosition = oldPosition;
+        oldRotation = transform.rotation;
+        currentRotation = oldRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 250.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        currentPosition = transform.position;
+        currentRotation = transform.rotation;
+
+        if (currentPosition != oldPosition)
+        {
+            NetworkManager.instance.GetComponent<NetworkManager>();
+            oldPosition = currentPosition;
+        }
+
+        if (currentRotation != oldRotation)
+        {
+            NetworkManager.instance.GetComponent<NetworkManager>();
+            oldRotation = currentRotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NetworkManager n = NetworkManager.instance.GetComponent<NetworkManager>();
+            CmdFire();
+        }
+    }
+
+    public void CmdFire()
+    {
+        var bullet = Instantiate(bulletPrefab,
+                                 bulletSpawn.position,
+                                 bulletSpawn.rotation) as GameObject;
+
+        Bullet b = bullet.GetComponent<Bullet>();
+        //b.playerFrom = this.gameObject;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 9;
+
+        Destroy(bullet, 2.0f);
+
+    }
+}
